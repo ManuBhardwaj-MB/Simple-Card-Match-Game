f8dae1314d57ff58706eb83e731c207279859f9e
Manu Bhardwaj <manubhardwaj@Manus-MBP.cust.communityfibre.co.uk>
2024-06-08 13:04:01 +0100

    Added-Settings-Menu
    Added setting for difficulty and sfx

---
+++ Assets/Project/Scripts/Views/SfxSettingsView.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SfxSettingsView : MonoBehaviour
+{
+    [SerializeField] private Color onLightColor;
+    [SerializeField] private Color offLightColor;
+    [SerializeField] private Image activeIndicatorImage;
+    
+    private bool isSubscribed;
+    
+    private void OnEnable()
+    {
+        if (!isSubscribed)
+        {
+            SettingsManager.Instance.OnSfxSettingsChanged += ShowSettingsUI;
+            isSubscribed = true;
+        }
+
+        ShowSettingsUI(SettingsManager.Instance.GetSfxSettings());
+    }
+
+    private void OnDisable()
+    {
+        if (isSubscribed)
+        {
+            SettingsManager.Instance.OnSfxSettingsChanged -= ShowSettingsUI;
+            isSubscribed = false;
+        }
+    }
+    
+    private void ShowSettingsUI(bool value)
+    {
+        activeIndicatorImage.color = value ? onLightColor : offLightColor;
+        GameManager.Instance.Log("UI >> Mainmenu >> SfxSettingsUI Updated: " + value);
+    }
+    
+    public void SetSettings()
+    {
+        SettingsManager.Instance.SetSfxOn(!SettingsManager.Instance.GetSfxSettings());
+    }
+}
